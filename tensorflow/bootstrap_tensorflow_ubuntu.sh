1977979394b59b966c5ab6ece5e960c9c9e0070f
Maashu <consulting@understructure.com>
2017-07-20 08:52:41 -0400

    Added notebook for COMPLETED Unit 2 of Udacity Deep Learning course (ud 730) and bootstrap script for EC2 (not quite perfect)

--- tensorflow/bootstrap_tensorflow_ubuntu.sh
+++ tensorflow/bootstrap_tensorflow_ubuntu.sh
@@ -9,11 +9,24 @@ apt-get upgrade && apt-get update
 apt-get install -y python3-pip python3-dev
 
 # NOTE: These alias commands don't work in bootstrap script??
-alias python=python3
-alias pip=pip3
-
+echo "alias python=python3" >> /home/ubuntu/.profile
+echo "alias pip=pip3" >> /home/ubuntu/.profile
 
 # NOTE:  Put a check in here to determine if we've got an NVDIA GPU, if so,
 # install tensorflow-gpu, else, install tensorflow
 
 pip3 install tensorflow
+pip3 install pandas
+pip3 install matplotlib seaborn
+
+apt-get install -y python3-tk
+
+apt-get install -y awscli
+
+aws s3 cp s3://ustruct-code/tensorflow/trainer.py .
+aws s3 cp s3://ustruct-code/tensorflow/distributed_tf.sh .
+
+chmod a+x distributed_tf.sh
+chmod +x trainer.py
+
+
